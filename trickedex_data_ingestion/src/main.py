80b40649f00fe860943b39d255fce960f54a109f
Dylan James <alazyartist@gmail.com>
2023-04-16 23:08:36 -0600

    supervisely

--- trickedex_data_ingestion/src/main.py
+++ trickedex_data_ingestion/src/main.py
@@ -1,10 +1,22 @@
 import time
+from dotenv import load_dotenv
+import os
+import supervisely as sly
+
 
 
 def main():
-    while True:
-        print("loop sleep")
-        time.sleep(60 * 60)  # sleep one hour
+    if sly.is_development():
+        load_dotenv("local.env")
+        load_dotenv(os.path.expanduser("./../supervisely.env"))
+    api = sly.Api.from_env(env_file='./../supervisely.env')
+    
+    
+    print(f"loaded {api}")
+
+    workspace_id = sly.env.workspace_id()
+    project = api.project.create(workspace_id,"Tricks",type=sly.ProjectType.VIDEOS, change_name_if_conflict=True)
+    print(project)
 
 
 if __name__ == "__main__":
